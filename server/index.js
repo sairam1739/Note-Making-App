9fcbaba3b49bb52802005ddcad7c25d0683b4d62
sairam1739 <117503218+sairam1739@users.noreply.github.com>
2025-02-03 10:14:49 +0530

    vercel started

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import noteRoutes from "./routes/noteRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 
 const app = express()
-
+const PORT = process.env.PORT || 4000;
 app.use(cors())
 app.use(express.json())
 app.use(listener)
@@ -24,7 +24,7 @@ async function Start() {
     try {
         mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected")).catch(err => console.error("MongoDB connection error:", err));
         app.listen(process.env.PORT, () => {
-            console.log(`Server started on port ${process.env.PORT}`)
+            console.log(`Server started on port ${PORT}`)
         })
     }
     catch (err) {
